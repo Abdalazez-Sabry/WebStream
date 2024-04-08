44cbe3f4b8f867113bbc19347b49dd79d88406d8
Abdalazez-Sabry <abdalazezsabry00@gmail.com>
2024-04-08 04:57:08 +0200

    init

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WidnowsAndroidStream;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace WidnowsAndroidStream;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
